20ea7136ce903697ec6368d6cc08bd64fd1b1541
olga <cref2015@i.ua>
2016-10-08 13:15:18 +0300

    finish

--- posts/views.py
+++ posts/views.py
@@ -25,6 +25,7 @@ from comments.forms import CommentForm
 from comments.models import Comment
 from .models import Post
 from .forms import PostForm
+from .utils import get_read_time
 
 
 def post_create(request):
@@ -52,24 +53,40 @@ def post_detail(request, slug=None):
         if not request.user.is_staff or not request.user.is_superuser:
             raise Http404
     share_string = quote_plus(instance.content)
+
+    # print(get_read_time(instance.content))
+    print(get_read_time(instance.get_markdown()))
+    
     initial_data ={
         "content_type": instance.get_content_type,
         "object_id": instance.id,
         }
     form = CommentForm(request.POST or None, initial=initial_data)
-    if form.is_valid():
+    if form.is_valid() and request.user.is_authenticated():
         c_type = form.cleaned_data.get("content_type")
         content_type = ContentType.objects.get(model=c_type)
         obj_id = form.cleaned_data.get("object_id")
         content_data = form.cleaned_data.get("content")
+        parent_obj = None
+        try:
+            parent_id = int(request.POST.get("parent_id"))
+        except:
+            parent_id = None
+        if parent_id:
+            parent_qs = Comment.objects.filter(id=parent_id)
+            if parent_qs.exists() and parent_qs.count() == 1:
+                parent_obj = parent_qs.first()
+
+        
         new_comment, created = Comment.objects.get_or_create(
                             user = request.user,
                             content_type = content_type,
                             object_id = obj_id,
                             content = content_data,
+                            parent = parent_obj,
                         )
-        if created:
-            print("work")
+        # что бы форма с комментами очищалась после отправки коммента
+        return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
 
     comments = instance.comments #Comment.objects.filter_by_instance(instance)
     context = {
